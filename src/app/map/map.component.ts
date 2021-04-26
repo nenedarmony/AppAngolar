0e78066fc79fab4bf254bf22b712157080de9f3f
nenedarmo@gmail.com <nenedarmo@gmail.com>
2021-04-26 12:58:28 +0300

    google maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,14 @@
-import {} from 'googlemaps';
-import { Component, OnInit } from '@angular/core';
-
+import { } from 'googlemaps';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  NgZone
+} from '@angular/core';
+import {
+  MapsAPILoader
+} from '@agm/core';
 
 @Component({
   selector: 'app-map',
@@ -8,9 +16,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+ 
+  latitude: number;
+  longitude: number;
+  zoom:number;
+  address: string;
+  private geoCoder;
 
-  constructor() { }
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+
 
   ngOnInit() {
+
+    const pyrmont = { lat: -33.866, lng: 151.196 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: pyrmont,
+      zoom: 17,
+      mapId: "6a2a54cc8596dad4",
+    });
+  
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement,{ componentRestrictions: {country: 'isr'}}
+        );
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
   }
+
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
 }
+
+
+
